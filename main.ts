6084abc9eb2c4722b93190e95a6de98332dfbb85
Signynt <67801159+Signynt@users.noreply.github.com>
2021-10-18 23:27:24 +0200

    Removed unecessary code, and updated some variables.

--- main.ts
+++ main.ts
@@ -3,25 +3,24 @@ import { App, Plugin, MarkdownPostProcessor, MarkdownPostProcessorContext } from
 export default class LinkHeadersDirectly extends Plugin {
 
 	async onload() {
-    let postProc: MarkdownPostProcessor;
+
+		let postProc: MarkdownPostProcessor;
 
 		postProc = (el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
 
-	    var linkElements = el.querySelectorAll('a.internal-link');
-	    var barIndex, alias;
-	    for(var i = 0; i < linkElements.length; i++) {
-		    barIndex = (linkElements[i] as HTMLElement).innerText.indexOf(">");
-		    if(barIndex < 0) continue;
-		    alias = (linkElements[i] as HTMLElement).innerText.substr(barIndex+2);
-		    (linkElements[i] as HTMLElement).innerText = alias;
-	    }
+			let linkElements = el.querySelectorAll('a.internal-link');
+			let barIndex, alias;
 
-    }
+			for(var i = 0; i < linkElements.length; i++) {
 
-	this.registerMarkdownPostProcessor(postProc);
-}
+				let linkAsHTML = (linkElements[i] as HTMLElement).innerText;
 
-	onunload() {
-		console.log('unloading plugin');
+				barIndex = linkAsHTML.indexOf(">");
+				if(barIndex < 0) continue;
+				alias = linkAsHTML.substr(barIndex+2);
+				(linkElements[i] as HTMLElement).innerText = alias;
+			}
+		}
+		this.registerMarkdownPostProcessor(postProc);
 	}
 }
